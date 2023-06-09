ce604d00dc11205db336c4af13af18e17fa55e54
Tetiana-Nazarenko <nnn27725@gmail.com>
2023-06-09 21:53:30 +0300

    add task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,31 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+function createElementsCard(galleryItems) {
+    return galleryItems
+        .map(({ original, preview, description }) => {
+            return `
+        <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+        />
+    </a>
+</li>
+        `;
+        })
+        .join('');
+}
+console.log(createElementsCard(galleryItems));
+const item = document.querySelector('.gallery');
+const cardsMarkup = createElementsCard(galleryItems);
+item.insertAdjacentHTML('beforeend', cardsMarkup);
+/*** */
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
